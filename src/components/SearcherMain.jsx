03c3678774b20deefa4e8645c8390323b9e9d07d
Santiago Montoya Cano <santiagomontoya98@hotmail.com>
2021-11-28 17:48:56 -0500

    Agregando renderizado condicional de Logout

--- src/components/SearcherMain.jsx
+++ src/components/SearcherMain.jsx
@@ -1,11 +1,26 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import { ContainerMain, FormStyle, HeaderContainer, ImgLupa, LensContainer, PCancelar, PLens, PProducts, Product, ProductDescription, ProductName, ProductPrice, Products, ProductsContainer, SearcherStyle } from '../styles/SearcherMainStyles'
-import { allProducts } from '../data/products'
-import Logout from './Logout'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  ContainerMain,
+  FormStyle,
+  HeaderContainer,
+  ImgLupa,
+  LensContainer,
+  PCancelar,
+  PLens,
+  PProducts,
+  Product,
+  ProductDescription,
+  ProductName,
+  ProductPrice,
+  Products,
+  ProductsContainer,
+  SearcherStyle,
+} from "../styles/SearcherMainStyles";
+import { allProducts } from "../data/products";
+import Logout from "./Logout";
 
-const SearcherMain = () => {  
-
+const SearcherMain = () => {
   const [products, setProducts] = useState([]);
 
   const [title, setTitle] = useState("");
@@ -17,13 +32,10 @@ const SearcherMain = () => {
   const [p, setP] = useState("Realiza una búsqueda");
 
   const getProducts = (name) => {
-
-    if(name !== undefined){
-
+    if (name !== undefined) {
       name = name.toLowerCase();
 
-      if(name === ""){
-
+      if (name === "") {
         setProducts([]);
 
         setTitle("");
@@ -33,27 +45,24 @@ const SearcherMain = () => {
         setDisplay2("");
 
         setP("Realiza una búsqueda");
-
-      }else{
-
+      } else {
         setDisplay2("none");
 
         setDisplay1("");
 
-        if(name.includes("guaj")){
-
-          setProducts(allProducts.filter(p => p.name.toLowerCase().includes(name)))          
-
-        }else{
-
-          setProducts(allProducts.filter(p => p.name2.toLowerCase().includes(name)))
-
+        if (name.includes("guaj")) {
+          setProducts(
+            allProducts.filter((p) => p.name.toLowerCase().includes(name))
+          );
+        } else {
+          setProducts(
+            allProducts.filter((p) => p.name2.toLowerCase().includes(name))
+          );
         }
 
-        if(products[0] !== undefined) setTitle(products[0].category);
+        if (products[0] !== undefined) setTitle(products[0].category);
 
-        if(products.length === 0) {
-
+        if (products.length === 0) {
           setTitle("");
 
           setDisplay1("none");
@@ -61,88 +70,59 @@ const SearcherMain = () => {
           setDisplay2("");
 
           setP("No hay resultados");
-
         }
-
       }
-
     }
+  };
 
-  }
-  
-
-  const handleChange = ({target}) => getProducts(target.value);
+  const handleChange = ({ target }) => getProducts(target.value);
 
   return (
-
-    <ContainerMain>      
-
+    <ContainerMain>
       <HeaderContainer>
-
         <FormStyle>
-
           <ImgLupa src="https://i.imgur.com/ooNjXdu.png" alt="Lupa" />
-          <SearcherStyle 
-            autoFocus 
+          <SearcherStyle
+            autoFocus
             type="text"
             name="searchText"
-            placeholder="Sabor de Guajolo..."            
-            onChange={handleChange}/>
-        
+            placeholder="Sabor de Guajolo..."
+            onChange={handleChange}
+          />
         </FormStyle>
 
         <Link to="/" className="cancel">
-        
           <PCancelar>Cancelar</PCancelar>
-
-        </Link>        
-
+        </Link>
       </HeaderContainer>
 
-      <ProductsContainer style={{display: display1}}>
-
+      <ProductsContainer style={{ display: display1 }}>
         <PProducts>{title}</PProducts>
 
         <Products>
-
-            {
-
-              products.map(el => (
-
-                <Link key={el.name+el.price} to={el.url} className="products">
-                
-                  <Product >
-                    <img src={el.img} alt={el.name} />
-                    <ProductDescription>
-                      <ProductName>{el.name2}</ProductName>
-                      <ProductPrice>${el.price} MXN</ProductPrice>
-                    </ProductDescription>
-                  </Product>
-
-                </Link>
-
-              ))
-
-            }
-
+          {products.map((el) => (
+            <Link key={el.name + el.price} to={el.url} className="products">
+              <Product>
+                <img src={el.img} alt={el.name} />
+                <ProductDescription>
+                  <ProductName>{el.name2}</ProductName>
+                  <ProductPrice>${el.price} MXN</ProductPrice>
+                </ProductDescription>
+              </Product>
+            </Link>
+          ))}
         </Products>
-
       </ProductsContainer>
 
-      <LensContainer style={{display: display2}}>
-
+      <LensContainer style={{ display: display2 }}>
         <img src="https://i.imgur.com/tmDKG0o.png" alt="" />
 
         <PLens>{p}</PLens>
-
       </LensContainer>
 
-      <Logout />
-
+      {localStorage.getItem("user") !== null ? <Logout /> : ""}
     </ContainerMain>
+  );
+};
 
-  )
-}
-
-
-export default SearcherMain
+export default SearcherMain;
